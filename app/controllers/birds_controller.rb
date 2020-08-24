823f2d9a7f89a146e9e83acb949b88ccd587cd11
Elijah wines <elijahwines84@yahoo.com>
2020-08-24 13:55:40 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render json: { message: "hashes of data will gte converted to json" } 
+    render json: { message: "hashes of data will get converted to json" } 
   end
 end
